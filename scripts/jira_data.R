f0f4bc3de0e9aa1babc9da48f7a605a86aec0707
dp24 <damonlbp@hotmail.co.uk>
2021-01-29 09:38:38 +0000

    Updates to code and evidence

--- scripts/jira_data.R
+++ scripts/jira_data.R
@@ -10,40 +10,13 @@ require("stringr")
 require('dplyr')
 require('gridExtra')
 library(tidyr)
-library(plotly)
-
-# Arg Handling
-args <- commandArgs(trailingOnly = TRUE)
-
-if (length(args) < 1){
-  warning("\nUSAGE: Rscript jira_data.R JIRA_DUMP_FILE SAVE_LOCATION \n")
-  stop("Must use both arguments")
-}
-
-cat(paste("Arguments are", args[1], args[2], "\n"))
-
-if (!file.exists(args[1])) {
-  stop("Can't find one of the input files")
-}
+require("plotly")
 
 # File Handling
 filename <- args[1]
-save_loc <- args[2]
 
-setwd("~/grit-realtime/output") # Change to save_loc in future
+setwd("../output") # Change to save_loc in future
 getwd()
-
-# Get data - May change to Jira pull in future
-jira_data <- read.csv('../output/jira_dump_250121.tsv.sorted', sep='\t', header=T, row.names=NULL)
-jira_data
-
-# Pull prefix for use downstream
-attach(jira_data)
-jira_data$prefix <- str_extract(jira_data$X.sample_id, '[[:lower:]]+') # pulls first letters for use as categorisers
-sapply(jira_data, class)  # to check data types
-jira_data$length.change <- as.numeric(as.character(jira_data$length.change)) # Stop gap measure
-jira_data <- head(jira_data, -1)
-
 
 # Dict which holds all prefixs
 master_dict = list('Amphibian' = 'a',
@@ -61,27 +34,46 @@ master_dict = list('Amphibian' = 'a',
                    'Shark' = 's',
                    'x' = 'x')
 
-# Could make this into a second generator for .change columns?
-plots_length <- function(dataframe) {
-  for (i in master_dict) {
-    print(i)
-    #if (nrow(current_df) > 2) {
-    ggplot(data=jira_data,
-          aes(x=X.sample_id, y=length.after, fill=prefix)) +
-      geom_bar(stat = 'identity') +
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1))
+date <- format(Sys.Date(), "%d%m%y")
+Jira_dump_file <- sprintf("./jira_dump_260121.tsv.sorted", date)
 
-   # } 
-    print(graph[[i]])
-    ggsave(graph[[i]],
-           file=paste0("plot_", i,".png"),
-           width = 44.45,
-           height = 27.78,
-           units = "cm",
-           dpi=300)
-  }
+main <- function(Jira_dump_file) {
+  # File Handling
+  setwd("../output") # Change to save_loc in future? That would be for none autonomous usecase
+  getwd()
+  
+  # Get data - May change to Jira pull in future
+
+  jira_data <- read.csv(Jira_dump_file, sep='\t', header=T, row.names=NULL)
+  attach(jira_data)
+  
+  # Pull prefix for use downstream
+  jira_data$prefix <- str_extract(jira_data$X.sample_id, '[[:lower:]]+') # pulls first letters for use as categorisers
+  jira_data$length.change <- as.numeric(as.character(jira_data$length.change)) # Stop gap measure
+  jira_data <- head(jira_data, -1)
+  return(jira_data)
 }
+
+jira_data <- main(Jira_dump_file)
+
+plots_length <- function(dataframe) {
+  # Facet wrapped plots for change in length for all grouped by prefix
+  ggplot(data=jira_data,
+         aes(x=X.sample_id, y=length.change, fill=prefix)) +
+    geom_bar(stat = 'identity') +
+    theme_minimal() +
+    theme(text = element_text(size=10),
+          axis.text.x = element_text(angle = 90, hjust = 1)) +
+    labs(x = "Project",
+         y = "Percentage change in length of genome post-curation",
+         title = "A graph to show the change in genome size when comparing pre and post curation") +
+    facet_wrap(~prefix , scales = "free", nrow = 3, ncol = 5)
+  ggplotly()
+  this_plot <- plotly::ggplotly()
+  htmlwidgets::saveWidget(as_widget(this_plot), 'plot_length_all.html')
+
+}
+
 plots_length(jira_data)
 
 box_plot <- function(dataframe) {
@@ -96,10 +88,10 @@ box_plot <- function(dataframe) {
 box_plot(jira_data)
 
 plots_master <- function(dataframe) {
-  attach(jira_data)
-  
-  con_graph <- ggplot(data=jira_data) +
-    geom_bar(aes(x=X.sample_id, y=length.after, fill=prefix),
+  # Shows all tickets and their change in genome length
+  con_graph <- 
+  ggplot(data=jira_data) +
+    geom_bar(aes(x=X.sample_id, y=length.change, fill=prefix),
              stat = "identity",
              position = "dodge") +
     theme_minimal()  +
@@ -107,68 +99,52 @@ plots_master <- function(dataframe) {
           axis.text.x = element_text(angle = 90,
                                      hjust = 1),
           axis.line = element_blank(),
-          axis.ticks = element_blank()) +
+          axis.ticks = element_blank())
+  ggplotly()+
     labs(x = "Project",
-         y = "Change in Length of Genome (bp) log10 scale",
-         title = "A graph to show the change in genome size post curation")
-  ggplotly()
-
-  log_plot <- ggplot(data=jira_data) +
-    geom_bar(aes(x=row.names, y=length.after, fill=prefix),
-             stat = "identity",
-             position = "dodge") +
-    theme_minimal()  +
-    theme(text = element_text(size=10),
-          axis.text.x = element_text(angle = 90,
-                                     hjust = 1),
-          axis.line = element_blank(),
-          axis.ticks = element_blank()) +
-    labs(x = "Project",
-         y = "Change in Length of Genome (bp)",
-         title = "A graph to show the change in genome size post curation")
-  ggplotly()
-
+         y = "Percentage change in Length of Genome",
+         title = "A graph to show the change in genome size pre compared to post curation")
+  master_plot <- plotly::ggplotly()
+  htmlwidgets::saveWidget(as_widget(master_plot), 'master_plot_len_change.html')
 }
 
 plots_master(jira_data)
 
-master_plot_change <- function() {
+mean_change_all <- function(datafram) {
+  # Plots the average genome length change for all groups
   dfs <- data.frame(x = character(), y = numeric(), stringsAsFactors = FALSE)
   for(i in master_dict) {
     mean <- jira_data[ which(jira_data$prefix==i), ]
-    change <- sum(mean$length.after / nrow(mean))
-    print(change)
-    print(i)
+    change <- sum(mean$length.change / nrow(mean))
+
     dfs[i,] <- list(i, change)
   }
   ggplot(dfs,
          aes(x, y, fill=x)) +
     geom_bar(stat='identity') +
     theme_minimal() +
-    theme(axis.line.x = element_blank())
+    theme(axis.line.x = element_blank()) +
+    labs(x = "Project-average",
+         y = "Percentage change in length of genome post-curation",
+         title = "A graph to show the average change in genome size grouped by prefix")
   
-  ggplotly()
+  this_plot <- plotly::ggplotly()
+  htmlwidgets::saveWidget(as_widget(this_plot), 'mean_change_all.html')
 }
 
-master_plot_change()
+mean_change_all(jira_data)
 
-jira_data$length.after <- jira_data$length.after*100
-jira_data$length.after <- signif(jira_data$length.after, digits = 2)
-ggplot(data=jira_data,
-       aes(x=row.names, y=length.after, fill=prefix)) +
-  geom_bar(stat = 'identity') +
-  theme_minimal() +
-  theme(text = element_text(size=10),
-        axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(x = "Project",
-       y = "Change in Length of Genome (bp) log10 scale",
-       title = "A graph to show the change in genome size post curation for") +
-  facet_wrap(~prefix , scales = "free")
-ggplotly()
-
-ggplot(data = jira_data,
-       aes(x=length.change, y=length.before, colour=prefix, fill = prefix)) +
+scatter_change_by_length_all <- function(dataframe) {
+  ggplot(data = dataframe,
+       aes(x=((length.after - min(length.after)) / (max(length.after) - min(length.after))), y=length.change, colour=prefix, label = X.sample_id)) +
   geom_point() +
   theme(text = element_text(size=10),
         axis.text.x = element_text(angle = 90, hjust = 1))
-ggplotly()
+  ggplotly()
+
+}
+
+scatter_change_by_length_all(jira_data)
+
+
+
